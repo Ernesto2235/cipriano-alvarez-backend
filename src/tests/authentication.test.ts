570c6532af2013c7470277453918fbbac903265c
Ernesto2235 <ernesto22355@gmail.com>
2025-09-17 14:13:57 -0400

    removed security flaws

--- src/tests/authentication.test.ts
+++ src/tests/authentication.test.ts
@@ -9,33 +9,33 @@ import {server} from "../app"
 
 // this block of code is for test cases that are supposed to pass for the auth controller
 test("login function, should return a 200 status code", async ()=>{
-    let user = {email:"cipriano.alvarez223@gmail.com",password:"Stanleytobar3!",secret:process.env.APP_SECRET};
+    let user = {email:process.env.USER_EMAIL,password:process.env.USER_PASSWORD,secret:process.env.APP_SECRET};
     let response = await request(app).post("/auth/login").send(user);
     expect(response.statusCode).toBe(200);
     
 })
 test("login function should return an error code 403 for invalid secret",async  ()=>{
-    let user = {email:"cipriano.alvarez223@gmail.com",password:"Stanleytobar3!",secret:"12345"};
+    let user = {email:process.env.USER_EMAIL,password:process.env.USER_PASSWORD,secret:"12345"};
     let response = await request(app).post("/auth/login").send(user);
     expect(response.statusCode).toBe(403)
 })
 test("login function should return an error code 400 for no email",async  ()=>{
-    let user = {password:"Stanleytobar3!",secret:process.env.APP_SECRET};
+    let user = {password:process.env.USER_PASSWORD,secret:process.env.APP_SECRET};
     let response = await request(app).post("/auth/login").send(user);
     expect(response.statusCode).toBe(400)
 })
 test("login function should return an error code 400 for no password",async  ()=>{
-    let user = {email:"cipriano.alvarez223@gmail.com",secret:process.env.APP_SECRET};
+    let user = {email:process.env.USER_EMAIL,secret:process.env.APP_SECRET};
     let response = await request(app).post("/auth/login").send(user);
     expect(response.statusCode).toBe(400)
 })
 test("login function should return an error code 404 for user not found",async  ()=>{
-    let user = {email:"cipriano.alvarez3@gmail.com",password:"Stanleytobar3!",secret:process.env.APP_SECRET};
+    let user = {email:"cipriano.alvarez3@gmail.com",password:process.env.USER_PASSWORD,secret:process.env.APP_SECRET};
     let response = await request(app).post("/auth/login").send(user);
     expect(response.statusCode).toBe(404)
 })
 test("login function should return an error code 401 for invalid password",async  ()=>{
-    let user = {email:"cipriano.alvarez223@gmail.com",password:"password123!",secret:process.env.APP_SECRET};
+    let user = {email:process.env.USER_EMAIL,password:"password123!",secret:process.env.APP_SECRET};
     let response = await request(app).post("/auth/login").send(user);
     expect(response.statusCode).toBe(401)
 })
