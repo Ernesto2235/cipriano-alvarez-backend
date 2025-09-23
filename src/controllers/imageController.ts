f882ac5bf6e4e0181d5891f5bcab048d2b0ad560
Ernesto2235 <ernesto22355@gmail.com>
2025-09-23 14:16:07 -0400

    changed error in image controller

--- src/controllers/imageController.ts
+++ src/controllers/imageController.ts
@@ -4,18 +4,19 @@ import  Express  from "express"
 
 export async function getImage(req: Express.Request,res:Express.Response){
     try{
-    if(!req.params.filename){
-        return res.status(400).send("Filename is required");
-    }
-    const filename = req.params.filename;
-    const imagePath = process.env.IMAGE_PATH+filename
-
-    res.sendFile(imagePath,(err)=>{
-        if(err){
-            console.log("error sending image",err);
-            res.status.apply(404).send("image not found");
+        if(!req.params.filename){
+            return res.status(400).send("Filename is required");
         }
-    })}catch(error){
+        const filename = req.params.filename;
+        const imagePath = process.env.IMAGE_PATH+filename
+
+        res.sendFile(imagePath,(err)=>{
+            if(err){
+                console.log("error sending image",err);
+                res.status(404).send("image not found");
+            }
+        })
+    }catch(error){
         res.status(500).send("Server error");
     }
 
